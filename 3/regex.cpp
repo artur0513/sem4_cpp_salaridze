ac708eb84cd52404cab59eb63ceaf4a0ca622e91
artur0513 <artur.salaridze@yandex.ru>
2023-03-08 18:28:09 +0300

    add '.' to email

--- 3/regex.cpp
+++ 3/regex.cpp
@@ -12,7 +12,7 @@ int main()
 	std::string input = buffer.str(); // file -> string bruh
 
 	std::smatch match;
-	std::regex email_regex("\\w+[@]{1}[A-Za-z]+[.]{1}[A-Za-z]{1,5}");
+	std::regex email_regex("[A-Za-z0-9_.]+[@]{1}[A-Za-z]+[.]{1}[A-Za-z]{1,5}");
 	while (std::regex_search(input, match, email_regex)) {
 		std::cout << match.str() << std::endl;
 		input = match.suffix();
